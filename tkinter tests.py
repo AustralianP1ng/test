75e1a153c6348a07ba51c46923814919956ca835
AustralianP1ng <156644623+AustralianP1ng@users.noreply.github.com>
2025-04-23 23:02:02 +0800

    tkinter tests
    -relearning gui

---
+++ tkinter tests.py
@@ -0,0 +1,11 @@
+# Playing around with tkinter
+
+import tkinter
+
+window = tkinter.Tk()
+
+window.geometry("420x420")
+window.title("Test")
+window.config(background= 'Black')
+
+window.mainloop()
